6610e00712093322f05987753e34b7406e6b2c35
Davis <20220354@ricaldone.edu.sv>
2025-10-15 18:32:13 -0600

    Cambio en el ingreso de los valores

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 def sumarValores(*valores):
     return sum(valores)
 
-print(sumarValores(10, 8))
+numeros = []
+
+for i in range(3):
+    ask = int(input("Coloca un numero para sumar: "))
+    numeros.append(ask)
+
+resultado = sumarValores(*numeros)
+print(f"Resultado: {resultado}")
